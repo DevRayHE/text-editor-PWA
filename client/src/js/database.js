010e5c59af3c49d86e9ae5fabf7fb1542b422c0d
DevRayHe <devrayhe@gmail.com>
2022-01-09 16:15:17 +1100

    Client side javascript
    Added logic to a method that accepts some content and adds it to the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,31 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  try {
+    console.log('Saving content to the database.');
+
+    // Create connection
+    const jateDb = await openDB('jate', 1);
+
+    // Create a new transaction and specify the database with data privileges.
+    const tx = jateDb.transaction('jate', 'readwrite');
+
+    // Open up the object store.
+    const store = tx.objectStore('jate');
+
+    // Use the .add() method on the store and pass in content.
+    const request = store.add({ content: content });
+
+    // Get confirmation of the request.
+    const result = await request;
+    console.log('data saved to the database', result);
+
+  } catch (err) {
+    console.error(`putDb not successful due to error: ${err}`);
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
